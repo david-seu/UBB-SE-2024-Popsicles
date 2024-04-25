caf42b15d637848036eb3284eb29ae42b79eb158
Stefan Macovei-Grigoras <stefan.macovei09@gmail.com>
2024-04-25 12:10:32 +0300

    n word

--- obj/Debug/net8.0-windows/Views/GroupView.g.i.cs
+++ obj/Debug/net8.0-windows/Views/GroupView.g.i.cs
@@ -160,13 +160,13 @@ namespace UBB_SE_2024_Popsicles.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UBB-SE-2024-Popsicles;component/views/groupview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UBB-SE-2024-Popsicles;V1.0.0.0;component/views/groupview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\GroupView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -176,14 +176,14 @@ namespace UBB_SE_2024_Popsicles.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal System.Delegate _CreateDelegate(System.Type delegateType, string handler) {
             return System.Delegate.CreateDelegate(delegateType, this, handler);
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
