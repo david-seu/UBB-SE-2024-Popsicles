1c77277851ab2a3dda5db69bf3ec2c8a3c3ac3f1
Lorintiu Daniel <lorintiu.daniel@yahoo.com>
2024-05-11 12:11:35 +0300

    .

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,8 @@
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 [assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly:InternalsVisibleTo("Test-UBB-SE-2024-Popsicles")]
  // where theme specific resource dictionaries are located
  // (used if a resource is not found in the page,
  // or application resource dictionaries)
